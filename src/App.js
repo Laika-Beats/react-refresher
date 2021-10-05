14513b9008429b601a560712899d6187963c5250
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-05 12:56:51 -0600

    useState

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ function App() {
   const author = "Joshua Thompson";
   const message = "test tweet text";
 
-  const sayHelloHandler = () => {
-    console.log("Hello");
-  };
   return (
     <div className="App">
-      <button onClick={sayHelloHandler}>CLICK</button>
       <CreateTweet />
       <TweetList author={author} message={message} />
     </div>

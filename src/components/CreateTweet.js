b04133f257931fdd58ecdfe48eeaf16e0058828b
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-05 13:09:38 -0600

    state

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const CreateTweet = () => {
+  const [textInput, setTextInput] = useState("");
+
+  const userInputHandler = (e) => {
+    setTextInput(e.target.value);
+  };
+
   return (
     <form>
-      <textarea cols="30" rows="5"></textarea>
+      <textarea
+        value={textInput}
+        onChange={userInputHandler}
+        cols="30"
+        rows="5"
+      ></textarea>
       <button>Submit</button>
     </form>
   );

a3913a302b8b12b2e1e182b017ef5eca8a0c4884
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-05 10:44:02 -0600

    events

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ function App() {
   // where you can write normal javascript
   const author = "Joshua Thompson";
   const message = "test tweet text";
+
+  const sayHelloHandler = () => {
+    console.log("Hello");
+  };
   return (
     <div className="App">
+      <button onClick={sayHelloHandler}>CLICK</button>
       <CreateTweet />
       <TweetList author={author} message={message} />
     </div>

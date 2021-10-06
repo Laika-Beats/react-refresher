36891d55f80cc80c769cc19027eebe289505e557
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-06 14:41:16 -0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TweetList from "./components/TweetList";
 import CreateTweet from "./components/CreateTweet";
 
 function App() {
   // where you can write normal javascript
+
   const [textInput, setTextInput] = useState("");
   const [tweets, setTweets] = useState([]);
 
   const author = "Joshua Thompson";
+
+  useEffect(() => {
+    console.log("YO");
+  }, [textInput]);
 
   return (
     <div className="App">
